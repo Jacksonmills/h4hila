60988474b0b9663a491462d9a17a40a787a674e0
Jacksonmills <jacksonemills@gmail.com>
2023-06-13 22:28:53 -0500

    basic post with fake buttons styled

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import { SignIn, SignedIn, SignedOut, UserButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
+import { CornerUpRight, Heart, type Icon } from "react-feather";
 
 import { api } from "~/utils/api";
 
@@ -12,7 +14,7 @@ const Home: NextPage = () => {
 
   const styles = {
     backgroundColor: "#002fff",
-    backgroundImage: "radial-gradient(at 17% 30%, #cc66ff 0, transparent 73%), radial-gradient(at 14% 95%, #2563eb 0, transparent 26%), radial-gradient(at 60% 26%, #3721b6 0, transparent 37%), radial-gradient(at 33% 87%, rgb(207, hsl(336.79245283018867, 52.475247524752476%, 60.3921568627451%)parent 54%), radial-gradient(at 32% 65%, #6fabff 0, transparent 44%), radial-gradient(at 53% 68%, #a0c0ff 0, transparent 26%)",
+    backgroundImage: "radial-gradient(at 17% 30%, #be1879 0, transparent 73%), radial-gradient(at 14% 95%, #2563eb 0, transparent 26%), radial-gradient(at 60% 26%, #3721b6 0, transparent 37%), radial-gradient(at 33% 87%, #cc66ff 0, transparent 54%), radial-gradient(at 32% 65%, #6fabff 0, transparent 44%), radial-gradient(at 53% 68%, #a0c0ff 0, transparent 26%)",
   };
 
   return (
@@ -23,23 +25,47 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center" style={styles}>
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-bold text-white">HOE<span className="text-[#cc66ff]">4</span>HILA</h1>
+        <div className="fixed top-0 w-screen flex items-center px-4 py-2">
+          <h1 className="text-4xl font-bold text-white pointer-events-none select-none">HOE<span className="text-[#cc66ff]">4</span>HILA</h1>
           <SignedOut>
             <SignIn />
           </SignedOut>
           <SignedIn>
-            <div>
-              <span><UserButton /></span>
-              <span>{user?.username}!</span>
-            </div>
-            <div>
-              {data?.map(({ id, content }) => (<div key={id}>{content}</div>))}
+            <div className="ml-auto">
+              <UserButton />
             </div>
           </SignedIn>
         </div>
-      </main>
+        <div className="bg-white text-black rounded-md">
+          {data?.map(({ id, content }) => {
+            return (
+              <div key={id} className="flex flex-col items-center justify-evenly gap-6 p-4">
+                <Image src={`https://i.redd.it/t6whec2wt6qa1.jpg`} width={446} height={594} alt="" className="rounded-t-md" />
+                <p className="text-current font-bold text-lg">{content}</p>
+                <div className="flex items-center justify-evenly gap-12">
+                  <ActionButton Icon={Heart} className="text-red-500" />
+                  <ActionButton Icon={CornerUpRight} className="text-blue-500" />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </main >
     </>
+  );
+};
+
+const ActionButton: React.FC<{ Icon: Icon; className: string; }> = ({ Icon, className }) => {
+  return (
+    <button
+      className={`
+        p-4 bg-white text-5xl rounded-full shadow-lg border ${className}
+        hover:scale-110 transform transition-all duration-200
+        active:scale-95
+      `}
+    >
+      <Icon className="w-[1em] h-[1em]" />
+    </button>
   );
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,6 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      'primary': '#c416f0',
-      'secondary': '#2751f7',
-      'tertiary': '#2e17e7',
-    },
     extend: {},
   },
   plugins: [],
