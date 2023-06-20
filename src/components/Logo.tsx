41c21aca662db0284d25fa702cffc2b09137838f
Jacksonmills <jacksonemills@gmail.com>
2023-06-20 00:25:19 -0500

    removed logo shadow ew

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Logo() {
   return (
-    <svg className='w-[84px] h-auto shadow-md' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1900.71 813.26">
+    <svg className='w-[84px] h-auto' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1900.71 813.26">
       <path className="fill-h3Pink" d="M1860.81,127c-21.91-50.02-86.21-89.32-168.04-102.71C1544.6,0,950.2,0,950.2,0c0,0-594.4,0-742.57,24.29C125.95,37.68,61.49,76.98,39.59,127,0,217.56,0,406.63,0,406.63c0,0,0,189.07,39.74,279.63,21.9,50.02,86.21,89.32,168.04,102.71,148.17,24.29,742.57,24.29,742.57,24.29,0,0,594.41,0,742.57-24.29,81.84-13.39,146.14-52.69,168.04-102.71,39.74-90.56,39.74-279.63,39.74-279.63,0,0-.16-189.06-39.9-279.63Z" />
       <polygon className="stroke-black" strokeMiterlimit={10} strokeWidth={4} points="1692.44 572.73 633.44 572.73 685.37 254.73 1604.87 254.73 1692.44 572.73" />
       <path className="stroke-black" strokeMiterlimit={10} strokeWidth={4} d="M218.77,572.35V254.62h59.91v123.18l118.01,16V254.62h59.91v317.73h-59.91v-124.08l-118.01-16v140.08h-59.91Z" />
