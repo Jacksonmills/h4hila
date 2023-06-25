05b64709190e3bea2df42f26240e3d98bcd88997
Jacksonmills <jacksonemills@gmail.com>
2023-06-25 13:24:32 -0500

    Update build configuration to ignore build errors, avoid linting during builds, and use SWC for minification.
    The config of the project has been changed to ignore build errors when building TypeScript files, ignore linting during builds and use SWC for minification resulting in a simplified configuration setup.

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,13 @@ const config = {
     locales: ['en'],
     defaultLocale: 'en',
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  swcMinify: true,
   images: {
     domains: [
       'i.redd.it',
