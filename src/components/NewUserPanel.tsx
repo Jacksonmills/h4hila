2cca639ab4ee7c366c0b9a7a37eac3dd483037ac
Jacksonmills <jacksonemills@gmail.com>
2023-06-25 21:27:22 -0500

    Refactor NewUserPanel to use ProfileFormCard
    This commit updates the NewUserPanel component to use a new child component called ProfileFormCard. This new component helps to simplify the code by abstracting the form and profile image elements into a reusable component. The new component takes several props and renders the form and profile image elements with their necessary styles. Overall, this change makes the code more modular and easier to read.

--- src/components/NewUserPanel.tsx
+++ src/components/NewUserPanel.tsx
@@ -1,16 +1,11 @@
-import { UserProfile, useUser } from '@clerk/nextjs';
-import Image from 'next/image';
+import { useUser } from '@clerk/nextjs';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
-import { Image as ImageIcon, XCircle, Send } from 'react-feather';
 
 import { api } from '~/utils/api';
 import { getRandomBrandColor } from '~/utils/getRandomBrandColor';
-import SaveButton from './SaveButton';
-import Modal from './Modal';
-
-import { motion } from 'framer-motion';
 import { toast } from 'react-hot-toast';
+import ProfileFormCard from './ProfileFormCard';
 
 export default function NewUserPanel() {
   const router = useRouter();
@@ -66,116 +61,18 @@ export default function NewUserPanel() {
   };
 
   return (
-    <div className='grid w-full place-content-center md:h-[90vh]'>
-      <div className='flex w-full max-w-[450px] flex-col gap-2 md:w-[705px] md:max-w-[705px] md:gap-6'>
-        <div className='flex flex-col rounded-2xl bg-white md:flex-row'>
-          <div className='relative'>
-            <div className='absolute bottom-2 right-2 md:bottom-4 md:right-4'>
-              <motion.button
-                whileHover={{ scale: 1.2 }}
-                whileTap={{ scale: 0.8 }}
-                transition={{ type: 'spring', stiffness: 300, damping: 20 }}
-                onClick={() => setModalOpen(!modalOpen)}
-                className='relative h-[50px] w-[50px] rounded-full border-2 border-black bg-h3LightBlue p-1 text-black'
-              >
-                <span className='pointer-events-none absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] text-current'>
-                  <ImageIcon />
-                </span>
-                <span className='sr-only'>
-                  Change profile image, opens dialog
-                </span>
-              </motion.button>
-            </div>
-            <Image
-              src={imageUrl}
-              width={446}
-              height={446}
-              alt={`${username}'s Profile Image`}
-              className='pointer-events-none h-[332px] w-full rounded-t-2xl border-2 border-black object-cover object-center md:h-full md:min-w-[332px] md:rounded-l-2xl md:rounded-tr-none'
-              style={{ backgroundColor: randomBackgroundColor }}
-            />
-          </div>
-          <div className='relative flex w-full grow flex-col gap-2 rounded-b-2xl border-2 border-t-0 border-black bg-h3Purple/20 p-2 md:rounded-r-2xl md:rounded-bl-none md:border-l-0 md:border-t-2 md:p-4'>
-            <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-              <div className='flex flex-col gap-1'>
-                <label htmlFor='name' className='flex gap-2 font-bold'>
-                  Username
-                </label>
-                <input
-                  type='text'
-                  className='rounded-xl border-2 border-black bg-white p-2 shadow-md'
-                  placeholder='Pick a display name'
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                />
-              </div>
-              <div className='flex flex-col gap-1'>
-                <label htmlFor='bio' className='flex gap-2 font-bold'>
-                  Bio
-                </label>
-                <textarea
-                  id='bio'
-                  className='h-[100px] resize-none rounded-xl border-2 border-black bg-white p-2 shadow-md md:h-[200px]'
-                  placeholder='Write a bio...'
-                  value={bio}
-                  onChange={(e) => setBio(e.target.value)}
-                />
-              </div>
-              <div className='flex justify-between'>
-                <p className='text-sm text-gray-400'>140 characters max</p>
-                <p
-                  className='text-sm text-gray-400 aria-disabled:text-red-500'
-                  aria-disabled={disabled}
-                >
-                  {bio.length}/140
-                </p>
-              </div>
-              <SaveButton type='submit' disabled={disabled}>
-                {disabled ? (
-                  <>
-                    <XCircle />
-                    <span className='sr-only'>Too long!</span>
-                  </>
-                ) : (
-                  <div className='flex items-center gap-4'>
-                    <Send />
-                    {isPosting ? 'Posting...' : 'Post'}
-                  </div>
-                )}
-              </SaveButton>
-            </form>
-          </div>
-        </div>
-      </div>
-      {modalOpen && (
-        <Modal
-          title='Change your profile picture'
-          handleDismiss={() => setModalOpen(false)}
-        >
-          <UserProfile
-            appearance={{
-              layout: {
-                logoPlacement: 'inside',
-              },
-              elements: {
-                header: 'hidden',
-                navbar: 'hidden',
-                navbarMobileMenuRow: 'hidden',
-                profileSectionTitle: 'hidden',
-                profileSection__connectedAccounts: 'hidden',
-                profileSection__password: 'hidden',
-                profileSection__activeDevices: 'hidden',
-                profilePage__security: 'hidden',
-                pageScrollBox:
-                  'p-2 md:min-h-[220px] flex gap-2 items-start justify-center',
-                page: 'w-full',
-                rootBox: 'm-0 w-full flex items-center justify-center',
-                card: 'rounded-2xl m-0 bg-white border-2 border-black',
-              },
-            }}
-          />
-        </Modal>
-      )}
-    </div>
+    <ProfileFormCard
+      username={username}
+      bio={bio}
+      imageUrl={imageUrl}
+      randomBackgroundColor={randomBackgroundColor}
+      modalOpen={modalOpen}
+      setModalOpen={setModalOpen}
+      disabled={disabled}
+      handleSubmit={handleSubmit}
+      setUsername={setUsername}
+      setBio={setBio}
+      isPosting={isPosting}
+    />
   );
 }
