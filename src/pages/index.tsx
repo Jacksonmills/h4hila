f07a7668795fba887173e5bda695152a8b488b11
Jacksonmills <jacksonemills@gmail.com>
2023-06-29 21:36:25 -0500

    refactor: Simplify data retrieval and loading condition in Home component
    This commit simplifies the data retrieval logic in the Home component by using object destructuring assignment for the `data` and `isLoading` variables in the `api.posts.getAll.useQuery` hook. It also modifies the loading condition to check for both `!data` and `isLoading` instead of just checking `data.length` to determine whether to display the loading spinner.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,30 +2,16 @@ import { type NextPage } from 'next';
 import { type RouterOutputs, api } from '~/utils/api';
 import LoadingSpinner from '~/components/LoadingSpinner';
 import Deck from '~/components/Deck';
-import { useEffect, useState } from 'react';
 
 export type AllPostData = RouterOutputs['posts']['getAll'];
 export type PostWithUser = AllPostData[number] | undefined;
 
 const Home: NextPage = () => {
-  const [data, setData] = useState<PostWithUser[]>([]);
-  const query = api.posts.getAll.useQuery(undefined, {
+  const { data, isLoading } = api.posts.getAll.useQuery(undefined, {
     refetchOnWindowFocus: false,
   });
 
-  useEffect(() => {
-    const fetchPosts = () => {
-      setData(query.data || []);
-    };
-
-    fetchPosts();
-
-    return () => {
-      setData([]);
-    };
-  }, [query.data]);
-
-  if (data.length === 0) return <LoadingSpinner size={69} />;
+  if (!data || isLoading) return <LoadingSpinner size={69} />;
 
   return (
     <div className='md:flex md:flex-col md:items-center md:justify-center'>
